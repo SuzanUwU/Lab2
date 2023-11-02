116101ae6dfa1b17d0001707d9a102c0da398f3b
Suzan <supyae.22@ichat.sp.edu.sg>
2023-11-02 19:52:03 +0800

    Test Lab2 file is tested and passed, now committing to Local

--- Test_Lab2.py
+++ Test_Lab2.py
@@ -1,5 +1,7 @@
 import Lab2
+
 print("Test_Lab2")
+
 
 def test_find_min_max():
     result = []
@@ -8,12 +10,14 @@ def test_find_min_max():
     result = Lab2.find_min_max(input_arr)
     assert (result == expected_result)
 
+
 def test_calc_average():
     result = []
     input_arr = [10, 20, 30, 40]
     expected_result = 25
     result = Lab2.calc_average(input_arr)
     assert (result == expected_result)
+
 
 def test_calc_median_temp():
     result = []
